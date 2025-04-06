55ada99b30cb638bbd1d0d0961c220bbd3298c58
Lorenzo Pagani <lorenzopagani012@gmail.com>
2025-04-07 00:03:20 +0200

    feat: add google drive api

---
+++ src/googleDrive.ts
@@ -0,0 +1,53 @@
+import dotenv from "dotenv";
+import { google } from "googleapis";
+
+dotenv.config();
+
+const SCOPES = [
+	"https://www.googleapis.com/auth/drive.readonly",
+	"https://www.googleapis.com/auth/drive.file",
+	"https://www.googleapis.com/auth/drive",
+];
+const DRIVE_FILE_ID = process.env.DRIVE_FILE_ID;
+
+const auth = new google.auth.GoogleAuth({
+	credentials: {
+		type: process.env.TYPE,
+		project_id: process.env.PROJECT_ID,
+		private_key_id: process.env.PRIVATE_KEY_ID,
+		private_key: process.env.PRIVATE_KEY?.replace(/\\n/g, "\n"),
+		client_email: process.env.CLIENT_EMAIL,
+		client_id: process.env.CLIENT_ID,
+		universe_domain: process.env.UNIVERSE_DOMAIN,
+	},
+	scopes: SCOPES,
+});
+
+const drive = google.drive({ version: "v3", auth });
+
+export const readJsonFromDrive = async () => {
+	const res = await drive.files.get(
+		{ fileId: DRIVE_FILE_ID, alt: "media", supportsAllDrives: true },
+		{ responseType: "stream" }
+	);
+
+	return new Promise<any>((resolve, reject) => {
+		let data = "";
+		res.data
+			.on("data", (chunk) => (data += chunk))
+			.on("end", () => resolve(JSON.parse(data)))
+			.on("error", reject);
+	});
+};
+
+export const writeJsonToDrive = async (jsonData: any) => {
+	const buffer = Buffer.from(JSON.stringify(jsonData, null, 2));
+
+	await drive.files.update({
+		fileId: DRIVE_FILE_ID,
+		media: {
+			mimeType: "application/json",
+			body: buffer,
+		},
+	});
+};
--- src/index.ts
+++ src/index.ts
@@ -1,46 +1,37 @@
 import cors from "cors";
+import dotenv from "dotenv";
 import express from "express";
-import fs from "fs";
 import nodemailer from "nodemailer";
+import { readJsonFromDrive, writeJsonToDrive } from "./googleDrive";
+
+dotenv.config();
 
 const app = express();
 const PORT = 3001;
-const FILE_PATH = "./src/guests.json";
-const EMAIL = "lorenzopagani012@gmail.com";
+const EMAIL = process.env.EMAIL;
+const EMAIL_PASS = process.env.EMAIL_PASS;
 
 app.use(express.json());
 app.use(cors());
 
 type Status = "ci sono" | "non ci sono" | null;
 
-// Tipi per TypeScript
 interface Guest {
 	id: number;
 	nome: string;
 	cognome: string;
 	status: Status;
+	note?: string;
 }
 
-// Leggere gli invitati dal file JSON
-const readGuests = (): Guest[] => {
-	return JSON.parse(fs.readFileSync(FILE_PATH, "utf8"));
-};
-
-// Scrivere gli invitati nel file JSON
-const writeGuests = (data: Guest[]) => {
-	fs.writeFileSync(FILE_PATH, JSON.stringify(data, null, 2));
-};
-
-// Configura il trasporto email
 const transporter = nodemailer.createTransport({
 	service: "gmail",
 	auth: {
 		user: EMAIL,
-		pass: "zdin eqqs guwv wjmk",
+		pass: EMAIL_PASS,
 	},
 });
 
-// Funzione per inviare l'email
 const sendEmail = (data: Guest[], guest?: Guest) => {
 	let statusDescription = "non si sa";
 
@@ -50,11 +41,19 @@ const sendEmail = (data: Guest[], guest?: Guest) => {
 		statusDescription = "è un super cattivo";
 	}
 
+	const presentCount = data.filter(
+		(inv: Guest) => inv.status === "ci sono"
+	).length;
+
 	const mailOptions = {
 		from: EMAIL,
 		to: EMAIL,
 		subject: "Aggiornamento Invitati",
-		text: `Ecco il file JSON aggiornato degli invitati.\n\n${guest?.nome} ${guest?.cognome}: ${statusDescription}\n\n`,
+		text: `Ecco il file JSON aggiornato degli invitati.\n\n${guest?.nome} ${
+			guest?.cognome
+		}: ${statusDescription}\n\nNote: ${
+			guest?.note ?? "Nessuna"
+		}\n\nNumero presenti aggiornato: ${presentCount}\n\n`,
 		attachments: [
 			{
 				filename: "guests.json",
@@ -64,36 +63,36 @@ const sendEmail = (data: Guest[], guest?: Guest) => {
 	};
 
 	transporter.sendMail(mailOptions, (error, info) => {
-		if (error) {
-			return console.log(error);
-		}
-		console.log("Email inviata: " + info.response);
+		if (error) return console.log(error);
+		console.log("📨 Email inviata: " + info.response);
 	});
 };
 
 // Endpoint per ottenere gli invitati
-app.get("/guests", (req, res) => {
-	res.json(readGuests());
+app.get("/guests", async (req, res) => {
+	const guests = await readJsonFromDrive();
+	res.json(guests);
 });
 
 // Endpoint per aggiornare lo stato di un invitato
-app.post("/confirm", (req, res) => {
+app.post("/confirm", async (req, res) => {
 	const { id, status, note } = req.body;
-	let updatedGuests = readGuests();
+	let updatedGuests = await readJsonFromDrive();
 
-	updatedGuests = updatedGuests.map((inv) =>
+	updatedGuests = updatedGuests.map((inv: Guest) =>
 		inv.id === id ? { ...inv, status, note } : inv
 	);
 
-	writeGuests(updatedGuests);
+	await writeJsonToDrive(updatedGuests);
+
 	sendEmail(
 		updatedGuests,
-		updatedGuests.find((inv) => inv.id === id)
+		updatedGuests.find((inv: Guest) => inv.id === id)
 	);
+
 	res.json({ message: "Conferma aggiornata!", guests: updatedGuests });
 });
 
-// Avvia il server
 app.listen(PORT, () => {
 	console.log(`✅ Server in ascolto`);
 });
